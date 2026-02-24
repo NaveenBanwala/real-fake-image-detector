ca24bc9311096502d314965c92597a1f61e9fb1b
Naveen Banwala <nbanwala7@gmail.com>
2026-02-24 07:52:52 +0530

    delete output.tf

--- real-fake-ai/terraform/output.tf
+++
@@ -1,9 +0,0 @@
-output "instance_ip" {
-  description = "The public IP of the web server"
-  value       = aws_instance.app_server.public_ip
-}
-
-output "security_group_id" {
-  description = "The ID of the new app security group"
-  value       = aws_security_group.app_sg.id
-}
